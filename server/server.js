aea5538e8513c031833ec07cd45f393086972147
Chris Cantoni <chris.cantoni@gmail.com>
2023-12-07 12:11:19 -0600

    Start of search API functionality.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
+import axios from 'axios';
 const express = require('express');
 const favoriteRouter = require('./routes/favorite.router');
 const categoryRouter = require('./routes/category.router');
+const searchRouter = require('./routes/search.router');
+
 
 const app = express();
 const PORT = process.env.PORT || 5001;
@@ -10,6 +13,19 @@ app.use(express.static('build'));
 
 app.use('/api/favorites', favoriteRouter);
 app.use('/api/categories', categoryRouter);
+app.use('/api/search', searchRouter);
+
+app.get('/:q', (req, res) => {
+  console.log(req.params.q);
+  axios.get(`https://api.giphy.com/v1/gifs/search?api_key=qU50qUPmY2TzvPcU5FXsYtYz4EdFcfsl&q=${req.params.q}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`)
+      .then((response) => {
+          res.send(response.data);
+      }).catch((error) => {
+          console.log('Search error', error);
+          res.sendStatus(500);
+      })
+})
+
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,35 @@
+import react, {useState} from 'react';
+import axios from 'axios';
+
 function App() {
+
+  let [searchTerm, setSearchTerm] = useState('');
+  let [searchResults, setSearchResults] = useState([])
+  
+function sendSearch() {
+  console.log(searchTerm);
+  axios.get(`https://api.giphy.com/v1/gifs/search?api_key=qU50qUPmY2TzvPcU5FXsYtYz4EdFcfsl&q=${searchTerm}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`)
+  .then((response) => {
+    console.log(response.data.data)
+    setSearchResults(response.data.data)
+  }).catch((err) => {
+    console.log('search error', err)
+    alert('Something went wrong!');
+  })
+}
+
   return (
     <div>
       <h1>Giphy Search!</h1>
+
+      <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
+      <button onClick={sendSearch}>Click me to search</button>
+      {/* {JSON.stringify(searchResults)} */}
+      <ul>
+        {searchResults.map((item) => {
+          return <li><img src={item.embed_url} /></li>
+        })}
+      </ul>
     </div>
   );
 }
